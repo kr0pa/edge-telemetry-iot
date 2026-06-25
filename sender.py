aa44ba199ebe205d9b8a03ea894b95b766a911e3
kr0pa <piotrkropagorski@gmail.com>
2026-06-25 12:34:22 +0200

    feat: added functionality of collecting processor load and RAM usage

--- sender.py
+++ sender.py
@@ -1,7 +1,9 @@
 import time
 import os
 import requests
+import psutil
 
+psutil.PROCFS_PATH = 'hostfs/proc'
 API_URL = "https://kr0pa.pl/api/receive/"
 
 def get_cpu_temperature():
@@ -19,15 +21,19 @@ def main():
     print("Uruchomiono Agenta Telemetrii (Tylko Temperatura CPU)...")
     while True:
         temp = get_cpu_temperature()
-        
+		cpu_load = psutil.cpu_percent(interval=1)
+		ram_usage = psutil.virtual_memory().percent
+
         payload = {
             "device_id": "raspberry-pi-edge",
-            "cpu_temp": temp
+            "cpu_temp": temp,
+			"cpu_load": cpu_load,
+			"ram_usage": ram_usage
         }
         
         try:
             response = requests.post(API_URL, json=payload, timeout=5)
-            print(f"Wysłano dane: Temperatura CPU: {temp}°C | Status API: {response.status_code}")
+            print(f"Wysłano dane: Temperatura CPU: {temp}°C | Obciążenie CPU: {cpu_load}% | Zużycie RAM: {ram_usage}% | Status API: {response.status_code}")
         except requests.exceptions.RequestException as e:
             print(f"Problem z połączeniem z API: {e}")
             
